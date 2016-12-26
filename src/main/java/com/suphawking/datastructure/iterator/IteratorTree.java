76e404717665ddc5c9788cdb1803a99a1f74a5d0
真人 <weizhen.wang@duitang.com>
2016-12-26 21:40:10 +0800

    feat: update

--- src/main/java/com/suphawking/datastructure/iterator/IteratorTree.java
+++ src/main/java/com/suphawking/datastructure/iterator/IteratorTree.java
@@ -65,17 +65,23 @@ public class IteratorTree implements Iterator {
     }
   }
 
+  //检查迭代器中是否还有剩余的元素
+  public boolean hasNext() {
+    return (null != nextPosition);
+  }
+
   //返回迭代器中的下一元素
   public Object getNext() throws ExceptionNoSuchElement {
-    if (!hasNext()) throw new ExceptionNoSuchElement("No next position");
+    if (!hasNext()) {
+      throw new ExceptionNoSuchElement("No next position");
+    }
     Position currentPosition = nextPosition;
     if (currentPosition == list.last())//若已到达尾元素，则
       nextPosition = null;//不再有下一元素
-    else{
+    else {
       //否则
       nextPosition = list.getNext(currentPosition);//转向下一元素
     }
-
     return currentPosition.getElem();
   }
 }
